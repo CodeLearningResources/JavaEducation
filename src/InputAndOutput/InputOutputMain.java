13d62e1a9bf32b69b00ad498dabc602ad612597d
Justin Kim <myungin.kim@mytrum.com>
2016-08-26 16:42:47 +0900

    Frame of the Ch 9 Input and Output.

--- src/InputAndOutput/InputOutputMain.java
+++ src/InputAndOutput/InputOutputMain.java
@@ -1,9 +1,39 @@
 package InputAndOutput;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  * Created by mikim on 2016-08-24.
  */
 public class InputOutputMain {
+
+
+    public static File produceTocFile(File file) {
+        /*
+        Ch 9.2
+        Read a text file file and produces a file with same name but extension .toc,
+        containing an alphabetized list of all words in the input file together with a list of line numbers in which
+        each word occurs.
+         */
+        
+    }
+
+    public static ArrayList<Integer> extractAllIntegers1(String input) {
+        /*
+            Ch 9. 10 (a)
+            Extract All decimal integers including negative ones from a String input to ArrayList<Integer> using find
+             */
+
+    }
+
+    public static ArrayList<Integer> extractAllIntegers2(String input) {
+        /*
+            Ch 9. 10 (a)
+            Extract All decimal integers including negative ones from a String input to ArrayList<Integer> using split
+             */
+
+    }
 
     public static void main(String[] args) {
 
